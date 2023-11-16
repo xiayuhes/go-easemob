3ab8e59a0f5b8bee5cebeb86cec3819cc9ad434b
Joy <gionna@qq.com>
2023-11-16 21:42:16 +0800

    fix: group add user

--- easemob_im/group.go
+++ easemob_im/group.go
@@ -138,20 +138,17 @@ func (s *Group) RemoveAdmin(groupId string, admin string) error {
 
 func (s *Group) AddUsername(groupId string, username string) error {
 	uri := s.auth.BuildURI(fmt.Sprintf("/chatgroups/%s/users/%s", groupId, username))
-	var res types.DataBoolResp
+	var res types.DataAnyResp
 	err := HttpPost(uri, nil, &res, s.auth.Headers())
 	if !gvar.New(err).IsEmpty() {
 		return err
 	}
-	if res.Data["result"] {
-		return nil
-	}
-	return err
+	return nil
 }
 
 func (s *Group) AddUsernames(groupId string, usernames []string) error {
 	uri := s.auth.BuildURI(fmt.Sprintf("/chatgroups/%s/users", groupId))
-	var res types.DataBoolResp
+	var res types.DataAnyResp
 	req := make(map[string]interface{})
 	req["usernames"] = usernames
 	err := HttpPost(uri, req, &res, s.auth.Headers())
@@ -163,20 +160,17 @@ func (s *Group) AddUsernames(groupId string, usernames []string) error {
 
 func (s *Group) RemoveUsername(groupId string, username string) error {
 	uri := s.auth.BuildURI(fmt.Sprintf("/chatgroups/%s/users/%s", groupId, username))
-	var res types.DataBoolResp
+	var res types.DataAnyResp
 	err := HttpDelete(uri, nil, &res, s.auth.Headers())
 	if !gvar.New(err).IsEmpty() {
 		return err
 	}
-	if res.Data["result"] {
-		return nil
-	}
-	return err
+	return nil
 }
 
 func (s *Group) RemoveUsernames(groupId string, usernames string) error {
 	uri := s.auth.BuildURI(fmt.Sprintf("/chatgroups/%s/users/%s", groupId, usernames))
-	var res types.DataBoolResp
+	var res types.DataAnyResp
 	err := HttpDelete(uri, nil, &res, s.auth.Headers())
 	if !gvar.New(err).IsEmpty() {
 		return err
